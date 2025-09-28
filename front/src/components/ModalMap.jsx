6fef1fc1fa1fe4cef807a3559b767a404df9dbf5
chanwoo1114 <cksfn14@gmail.com>
2025-09-28 14:49:47 +0900

    .

--- front/src/components/ModalMap.jsx
+++ front/src/components/ModalMap.jsx
@@ -1,14 +1,20 @@
+import NuclearImage from "../assets/images/typ_nuclear2.png"
+import ChemistryImage from "../assets/images/typ_chemistry2.png"
+import StormImage from "../assets/images/typ_storm2.png"
+import FloodImage from "../assets/images/typ_storm3.png"
+import ComplexImage from "../assets/images/typ_complex2.png"
+
 export default function ModalMap() {
   return (
     <div className="fixed inset-0 z-50">
       <div className="absolute inset-0 bg-black/50">
         <div className="absolute inset-0 flex items-center justify-center">
           <div className="rounded bg-white p-6">
-            <ul>
-              <li>
-
-              </li>
-            </ul>
+            <div className="flex items-center gap-3">
+              <img src={NuclearImage} alt="Nuclear Image" />
+              <img src={ChemistryImage} alt="Nuclear Image" />
+              <img src={StormImage} alt="Nuclear Image" />
+            </div>
           </div>
         </div>
       </div>
